47f7715ee507abe373e2274c35d8aedfd043b7b2
Mim <2639981498@qq.com>
2025-12-10 17:15:03 +0800

    wmm

--- src/main/java/com/boda/canteen/CanteenApplication.java
+++ src/main/java/com/boda/canteen/CanteenApplication.java
@@ -15,6 +15,6 @@ public class CanteenApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CanteenApplication.class, args);
-        log.info("canteen启动成功 (●'◡'●)");
+        //log.info("canteen启动成功 (●'◡'●)");
     }
 }
