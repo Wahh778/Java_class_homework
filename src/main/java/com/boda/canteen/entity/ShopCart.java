6ba7e9b5d859fee6657510202a452e0e739ca4d2
Mim <2639981498@qq.com>
2025-12-12 00:12:47 +0800

    wmm

--- src/main/java/com/boda/canteen/entity/ShopCart.java
+++ src/main/java/com/boda/canteen/entity/ShopCart.java
@@ -1,4 +1,5 @@
 package com.boda.canteen.entity;
+import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
@@ -12,7 +13,8 @@ public class ShopCart implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    @TableId
+    // 添加主键策略修改为自增
+    @TableId(type = IdType.AUTO)
     private Integer scId;
 
     private String name;
